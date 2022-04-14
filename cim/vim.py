4220a71e70b76a5be04b6c4b8c0afddd8c72b3ce
Vkud5f <103627279+Vkud5f@users.noreply.github.com>
2022-04-14 13:00:20 +0530

    Create vim.py

---
+++ cim/vim.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains  
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import tkinter as tk
+import secrets
+import random
+import string
+import os
+
+###TEMP-NAMES###
+
+t = random.randint(30,31)
+t1 = random.randint(200,350)
+t2 = random.randint(5,60)
+url_list= ['https://shortznewz.blogspot.com/2022/04/some-best-places-in-dubai.html?m=1', 'https://shortznewz.blogspot.com/2022/04/best-places-to-visit.html', 'https://shortznewz.blogspot.com/2022/04/facts-about-humans.html', 'https://shortznewz.blogspot.com/2022/04/random-facts.html', 'https://shortznewz.blogspot.com/2022/04/interesting-facts.html']
+url = random.choice(url_list)
+
+driver = webdriver.Chrome()
+time.sleep(t2)
+driver.get(url)
+time.sleep(t1)
+'''driver.find_element_by_xpath('//*[@id="skip_bu2tton"]/img').click()
+time.sleep(t1)'''
