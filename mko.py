f9aad14669892cf57d515229734de743a232cc61
Vkud5f <103627279+Vkud5f@users.noreply.github.com>
2022-04-14 22:23:07 +0530

    Create mko.py

---
+++ mko.py
@@ -0,0 +1,23 @@
+# ------------------------------------------------------------ #
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, WebDriverException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains  
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import os
+# ------------------------------------------------------------
+CHROMEDRIVER_PATH = os.environ.get('CHROMEDRIVER_PATH', '/usr/local/bin/chromedriver')
+GOOGLE_CHROME_BIN = os.environ.get('GOOGLE_CHROME_BIN', '/usr/bin/google-chrome')
+options = Options()
+options.binary_location = GOOGLE_CHROME_BIN
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.headless = True
+driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH , chrome_options=options)
+# ------------------------------------------------------------
+driver.get("https://shortznewz.blogspot.com/2022/04/some-best-places-in-dubai.html?m=1")
+time.sleep(300)
