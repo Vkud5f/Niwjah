fb8d3cefd2858dacb9a6ab88f3e4c4e2b77bbcc7
Vkud5f <103627279+Vkud5f@users.noreply.github.com>
2022-04-18 14:52:19 +0530

    Update vm.py

--- cim/vm.py
+++ cim/vm.py
@@ -36,7 +36,6 @@ chrome_options = webdriver.ChromeOptions()
 prefs = {"profile.default_content_setting_values.notifications": 1}
 chrome_options.add_experimental_option("prefs", prefs)
 driver = webdriver.Chrome(chrome_options=chrome_options)
-driver.maximize_window()
 driver.implicitly_wait(10)
 time.sleep(t)
 driver.get(url)
